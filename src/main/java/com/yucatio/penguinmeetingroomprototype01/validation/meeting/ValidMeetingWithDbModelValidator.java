20cac3866b859c23e58aecf8d3a928672b4020fc
yucatio <yucatio.com@gmail.com>
2022-09-18 17:59:22 +0900

    Added test and modify for test

--- src/main/java/com/yucatio/penguinmeetingroomprototype01/validation/meeting/ValidMeetingWithDbModelValidator.java
+++ src/main/java/com/yucatio/penguinmeetingroomprototype01/validation/meeting/ValidMeetingWithDbModelValidator.java
@@ -1,6 +1,10 @@
 package com.yucatio.penguinmeetingroomprototype01.validation.meeting;
 
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -10,17 +14,25 @@ import com.yucatio.penguinmeetingroomprototype01.validation.annotation.meeting.V
 
 public class ValidMeetingWithDbModelValidator
     implements ConstraintValidator<ValidMeetingWithDbModel, MeetingTransitionModel> {
+  private Clock clock = Clock.systemDefaultZone();
+
+  public ValidMeetingWithDbModelValidator() {
+  }
+  
+  // for unit test
+  public ValidMeetingWithDbModelValidator(Clock clock) {
+    this.clock = clock;
+  }
 
   @Override
   public boolean isValid(MeetingTransitionModel model, ConstraintValidatorContext context) {
     context.disableDefaultConstraintViolation();
+    
+    List<Boolean> result = Arrays.asList(
+        validateStartTime(model, context),
+        validateEndTime(model, context));
 
-    boolean isValid = true;
-
-    isValid &= validateStartTime(model, context);
-    isValid &= validateEndTIme(model, context);
-
-    return isValid;
+    return result.stream().allMatch(Boolean::valueOf);
   }
 
   protected boolean validateStartTime(MeetingTransitionModel model, ConstraintValidatorContext context) {
@@ -34,9 +46,11 @@ public class ValidMeetingWithDbModelValidator
       return true;
     }
 
+    Date now = Date.from(ZonedDateTime.now(clock).toInstant());
+
     boolean isValid = true;
 
-    if (model.getMergedModel().getStart().compareTo(new Date()) < 0) {
+    if (model.getMergedModel().getStart().compareTo(now) < 0) {
       // 過去
       context
           .buildConstraintViolationWithTemplate("{javax.validation.constraints.Future.message}")
@@ -44,7 +58,7 @@ public class ValidMeetingWithDbModelValidator
       isValid = false;
     }
 
-    if (model.getDbModel().getStart().compareTo(new Date()) < 0) {
+    if (model.getDbModel().getStart().compareTo(now) < 0) {
       // 開始時間を過ぎていたら開始時間を変更できない
       context
           .buildConstraintViolationWithTemplate(
@@ -57,7 +71,7 @@ public class ValidMeetingWithDbModelValidator
     return isValid;
   }
 
-  protected boolean validateEndTIme(MeetingTransitionModel model, ConstraintValidatorContext context) {
+  protected boolean validateEndTime(MeetingTransitionModel model, ConstraintValidatorContext context) {
     if (model.getDbModel() == null) {
       // insertは対象外
       return true;
@@ -67,10 +81,12 @@ public class ValidMeetingWithDbModelValidator
       // endTimeに変更がないなら問題なし
       return true;
     }
+    
+    Date now = Date.from(ZonedDateTime.now(clock).toInstant());
 
     boolean isValid = true;
 
-    if (model.getMergedModel().getEnd().compareTo(new Date()) < 0) {
+    if (model.getMergedModel().getEnd().compareTo(now) < 0) {
       // 過去
       context
           .buildConstraintViolationWithTemplate("{javax.validation.constraints.Future.message}")
@@ -78,12 +94,12 @@ public class ValidMeetingWithDbModelValidator
       isValid = false;
     }
 
-    if (model.getDbModel().getEnd().compareTo(new Date()) < 0) {
+    if (model.getDbModel().getEnd().compareTo(now) < 0) {
       // 終了時間を過ぎていたら終了時間を変更できない
       context
           .buildConstraintViolationWithTemplate(
               "{com.yucatio.penguinmeetingroomprototype01.validation.ValidMeetingWithDbModel.notChangeEndAfterEnd.message}")
-          .addPropertyNode("start").addConstraintViolation();
+          .addPropertyNode("end").addConstraintViolation();
       isValid = false;
     }
 
